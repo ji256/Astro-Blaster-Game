9d99a7ca229b695a6a1fb9e9d7dbcf18370c2d96
Jeremiah Ickom <143756467+ji256@users.noreply.github.com>
2025-10-08 17:18:30 -0500

    Some changes and additions to the code

--- AstroidBlasterGame/src/Main.java
+++ AstroidBlasterGame/src/Main.java
@@ -5,14 +5,13 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 import java.awt.event.*;
+
 public class Main {
 
-    private static int mouseX = 400, mouseY = 300;
-    private static JPanel panel;
 
     public static void main(String[] args) {
 
-        panel = new JPanel();
+
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(800, 600);
@@ -27,40 +26,12 @@ public class Main {
         Cursor blank = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank");
         window.setCursor(blank);
 
-
-        //Adds the ship that will be used in the game.
-        //Also moves based on input from the mouse or touchpad
-        panel = new JPanel() {
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                int[] xs = {mouseX, mouseX - 10, mouseX + 10};
-                int[] ys = {mouseY - 15, mouseY + 10, mouseY + 10};
-                g.setColor(Color.WHITE);
-                g.fillPolygon(xs, ys, 3);
-            }
-        };
-
-        panel.addMouseMotionListener(new MouseMotionAdapter() {
-            public void mouseMoved(MouseEvent e) {
-                mouseX = e.getX();
-                mouseY = e.getY();
-                panel.repaint();
-            }
-        });
-        panel.setFocusable(true);
-        panel.requestFocusInWindow();
-
-
-
-
-        panel.setBackground(Color.BLACK);
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
         window.setResizable(false);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        window.add(panel);
-
-
-
+        SwingUtilities.invokeLater(gamePanel::requestFocusInWindow);
 
 
         }
